d55851716dcec7404b851d4c8f181355aef8fadf
PhanNam1501 <tuannamle256@gmail.com>
2026-03-05 21:11:36 +0700

    fix fmt

--- src/challenge1/TickBitmap.sol
+++ src/challenge1/TickBitmap.sol
@@ -2,17 +2,14 @@
 pragma solidity ^0.8.20;
 
 import {BitMath} from "../libraries/Bitmath.sol";
+
 library TickBitmap {
     function position(int24 pos) private pure returns (int16 bitPos, uint8 wordPos) {
         bitPos = int16(pos >> 8);
         wordPos = uint8(int8(pos % 256));
     }
 
-    function flipTick(
-        mapping(int16 => uint256) storage self,
-        int24 tick,
-        int24 tickSpacing
-    ) internal {
+    function flipTick(mapping(int16 => uint256) storage self, int24 tick, int24 tickSpacing) internal {
         require(tick % tickSpacing == 0, "Not can use this tick");
         (int16 wordPos, uint8 bitPos) = position(tick / tickSpacing);
         uint256 mask = 1 << bitPos;
@@ -28,14 +25,14 @@ library TickBitmap {
         require(tick % tickSpacing == 0, "Not can use this tick");
         int24 compressed = tick / tickSpacing;
         if (tick < 0 && tick % tickSpacing != 0) compressed--;
-        
+
         if (lte) {
             (int16 wordPos, uint8 bitPos) = position(tick / tickSpacing);
             uint256 mask = (1 << bitPos) - 1 + (1 << bitPos);
             uint256 masked = mask & self[wordPos];
             initialized = masked != 0 ? true : false;
 
-            next =  initialized 
+            next = initialized
                 ? (compressed - int24(int8(bitPos - BitMath.mostSignificantBit(masked)))) * tickSpacing
                 : (compressed - int24(int8(bitPos))) * tickSpacing;
         } else {
